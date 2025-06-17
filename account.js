a2189bc35745c075bc655addf7c14039c8732ba1
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-17 14:40:45 +0530

    Add files via upload

---
+++ account.js
@@ -0,0 +1,5 @@
+  // Ensure the loader hides when the page fully loads
+  window.onload = function () {
+    // Add the 'loaded' class to the body to hide the loader
+    document.body.classList.add('loaded');
+  };
