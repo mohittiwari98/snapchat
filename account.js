bb61ae3433fc81ff77f3550dbeafc95a82ada324
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-17 14:46:28 +0530

    account.js

--- account.js
+++ account.js
@@ -1,4 +1,4 @@
-  // Ensure the loader hides when the page fully loads
+  // Ensure  loader hides when the page fully loads
   window.onload = function () {
     // Add the 'loaded' class to the body to hide the loader
     document.body.classList.add('loaded');
